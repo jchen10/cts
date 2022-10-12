12b2da7d4c156b141e56984da28c48d96d1d892d
jchen10 <jie.a.chen@intel.com>
2022-10-12 14:10:55 +0800

    Disable cases

--- src/webgpu/util/texture/texel_data.spec.ts
+++ src/webgpu/util/texture/texel_data.spec.ts
@@ -133,10 +133,14 @@ function makeParam(
   };
 }
 
+const unormFormats = kEncodableTextureFormats.filter(
+  format => format !== 'rgba8unorm-srgb' && format !== 'bgra8unorm-srgb'
+);
+
 g.test('unorm_texel_data_in_shader')
   .params(u =>
     u
-      .combine('format', kEncodableTextureFormats)
+      .combine('format', unormFormats)
       .filter(({ format }) => {
         return (
           kTextureFormatInfo[format].copyDst &&
@@ -306,6 +310,10 @@ TODO: Test NaN, Infinity, -Infinity [1]`
   )
   .fn(doTest);
 
+const ufloatFormats = kEncodableTextureFormats.filter(
+  format => format !== 'rg11b10ufloat' && format !== 'rgb9e5ufloat'
+);
+
 g.test('ufloat_texel_data_in_shader')
   .desc(
     `
@@ -313,7 +321,7 @@ TODO: Test NaN, Infinity [1]`
   )
   .params(u =>
     u
-      .combine('format', kEncodableTextureFormats)
+      .combine('format', ufloatFormats)
       .filter(({ format }) => {
         return (
           kTextureFormatInfo[format].copyDst &&
