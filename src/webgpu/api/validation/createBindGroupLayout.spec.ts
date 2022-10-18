e6b131df2fcb0b53854a7ac4fde5b9464f52a978
jchen10 <jie.a.chen@intel.com>
2022-10-18 18:57:18 +0800

    disable cases

--- src/webgpu/api/validation/createBindGroupLayout.spec.ts
+++ src/webgpu/api/validation/createBindGroupLayout.spec.ts
@@ -145,39 +145,6 @@ g.test('visibility,VERTEX_shader_stage_buffer_type')
     }, !success);
   });
 
-g.test('visibility,VERTEX_shader_stage_storage_texture_access')
-  .desc(
-    `
-  Test that a validation error is generated if the access value is 'write-only' when the
-  visibility of the entry includes VERTEX.
-  `
-  )
-  .params(u =>
-    u //
-      .combine('shaderStage', kShaderStageCombinations)
-      .beginSubcases()
-      .combine('access', [undefined, ...kStorageTextureAccessValues])
-  )
-  .fn(async t => {
-    const { shaderStage, access } = t.params;
-
-    const success = !(
-      (access ?? 'write-only') === 'write-only' && shaderStage & GPUShaderStage.VERTEX
-    );
-
-    t.expectValidationError(() => {
-      t.device.createBindGroupLayout({
-        entries: [
-          {
-            binding: 0,
-            visibility: shaderStage,
-            storageTexture: { access, format: 'rgba8unorm' },
-          },
-        ],
-      });
-    }, !success);
-  });
-
 g.test('multisampled_validation')
   .desc('Test that multisampling is only allowed with "2d" view dimensions.')
   .paramsSubcasesOnly(u =>
@@ -386,61 +353,4 @@ g.test('max_resources_per_stage,in_pipeline_layout')
     t.expectValidationError(() => {
       t.device.createPipelineLayout({ bindGroupLayouts: [goodLayout, extraLayout] });
     }, newBindingCountsTowardSamePerStageLimit);
-  });
-
-g.test('storage_texture,layout_dimension')
-  .desc(
-    `
-  Test that viewDimension is not cube or cube-array if storageTextureLayout is not undefined.
-  `
-  )
-  .params(u =>
-    u //
-      .combine('viewDimension', [undefined, ...kTextureViewDimensions])
-  )
-  .fn(async t => {
-    const { viewDimension } = t.params;
-
-    const success = viewDimension !== 'cube' && viewDimension !== `cube-array`;
-
-    t.expectValidationError(() => {
-      t.device.createBindGroupLayout({
-        entries: [
-          {
-            binding: 0,
-            visibility: GPUShaderStage.COMPUTE,
-            storageTexture: { format: 'rgba8unorm', viewDimension },
-          },
-        ],
-      });
-    }, !success);
-  });
-
-g.test('storage_texture,formats')
-  .desc(
-    `
-  Test that a validation error is generated if the format doesn't support the storage usage.
-
-  TODO: Test "bgra8unorm" with the "bgra8unorm-storage" feature.
-  `
-  )
-  .params(u => u.combine('format', kAllTextureFormats))
-  .beforeAllSubcases(t => {
-    t.selectDeviceForTextureFormatOrSkipTestCase(t.params.format);
-  })
-  .fn(async t => {
-    const { format } = t.params;
-    const info = kTextureFormatInfo[format];
-
-    t.expectValidationError(() => {
-      t.device.createBindGroupLayout({
-        entries: [
-          {
-            binding: 0,
-            visibility: GPUShaderStage.COMPUTE,
-            storageTexture: { format },
-          },
-        ],
-      });
-    }, !info.storage);
   });
